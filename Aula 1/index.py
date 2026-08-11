30e795b16257b8a3b0da1d3b718fe3ae50f0806d
nanaluvsu <worksender053@gmail.com>
2026-08-10 21:16:56 -0300

    Adicionando requisitos de bibliotecas e exercício 2.

--- Aula 1/index.py
+++ Aula 1/index.py
@@ -35,6 +35,61 @@ def ex1():
             break #ae break pode fazer 50000 video sobre o oliver tree
         
         else:
-             print("vc e burro smt")
+             print("vc e burra (ou burro sem preconceito)")
+
+def ex2():
+    '''
+    Escreva uma função que receba uma lista de números e retorne um dicionário contendo a contagem
+    de ocorrências de cada número na lista.
+    '''
+    def dict_qtd(lista):
+        dict_qtds = {}
+        lista = list(lista)
+        for item in lista:
+            if item not in dict_qtds:
+                dict_qtds[item] = lista.count(item)
+        return dict_qtds
+
+
+    lista_ex2 = [5,5,5,5,4,4,3,3,3,3,3,2,1,67]
+    print(dict_qtd(lista_ex2))
+
+def ex3():
+    '''
+    Considere o seguinte conjunto:
+    Z = {5, 2, 11, 8, 3, 8, 7, 4}
+    a) Crie um código em Python para realizar o cálculo do desvio padrão de Z sem utilizar bibliotecas e funções
+    matemáticas prontas para resolver o problema.
+    b) Utilize as funções disponíveis nas bibliotecas numpy e statistics e faça a conferência dos resultados
+    '''           
+
+    Z = [5,2,11,8,3,8,7,4]
+
+    
         
-ex1()
+print("oi qual exercicio vc quer")
+ex = int(input())
+
+match(ex):
+    case 1:
+        ex1()
+    case 2: 
+        ex2()
+    case 3: 
+        ex3()
+    case 4:
+        ex4()
+    case 5:
+        ex5()
+    case 6:
+        ex6()
+    case 7:
+        ex7()
+    case 8:
+        ex8()
+    case 9: 
+        ex9()
+    case 10:
+        ex10()
+    case _:
+        print("vc e burra (ou burro sem preconceito) smt")
